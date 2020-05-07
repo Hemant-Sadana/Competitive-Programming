263a7f553fe33e6d92f583b0cb155c721f78d631
Hemant Sadana <38503712+Hemant-Sadana@users.noreply.github.com>
2020-05-07 10:36:43 +0530

    Create Pascal Triangle.cpp

---
+++ Arrays/Pascal Triangle.cpp
@@ -0,0 +1,12 @@
+vector<vector<int> > Solution::solve(int A) {
+    vector<vector<int> > ans(A);
+    for(int i=0;i<A;i++)
+    {
+        ans[i] = vector<int> (i+1,1);
+        for(int j=1;j<i;j++)
+        {
+            ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+        }
+    }
+    return ans;
+}
