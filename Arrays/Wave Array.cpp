db6902890ccf9c844e6cd23ebb85f591aeb092e1
Hemant Sadana <38503712+Hemant-Sadana@users.noreply.github.com>
2020-05-07 11:35:06 +0530

    Create Wave Array.cpp

---
+++ Arrays/Wave Array.cpp
@@ -0,0 +1,9 @@
+vector<int> Solution::wave(vector<int> &A) {
+    int n = A.size();
+    sort(A.begin(),A.end());
+    for(int i=0;i<n-1;i=i+2)
+    {
+        swap(A[i],A[i+1]);
+    }
+    return A;
+}
