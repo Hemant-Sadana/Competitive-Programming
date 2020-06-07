95270cbd461a795a4a3f9ffa66db73cf4fbc7de5
Hemant Sadana <38503712+Hemant-Sadana@users.noreply.github.com>
2020-06-07 21:28:42 +0530

    Update 2-Sum Binary Tree.cpp

--- Tree Data Structure/2-Sum Binary Tree.cpp
+++ Tree Data Structure/2-Sum Binary Tree.cpp
@@ -7,27 +7,49 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-void store(TreeNode* A,int B,vector<int> &temp)
-{
-    if(A==NULL)
-    return;
-    store(A->left,B,temp);
-    if(A->val<=B)
-    temp.push_back(A->val);
-    store(A->right,B,temp);
-}
 int Solution::t2Sum(TreeNode* A, int B) {
-    vector<int> temp;
-    store(A,B,temp);
-    int n = temp.size(),i=0,j=n-1;
-    while(i<j)
+    if(A==NULL)
+    return 0;
+    stack<TreeNode*> s1,s2;
+    TreeNode* t1,*t2;
+    t1 = A;t2 = A;
+    while(t1!=NULL)
     {
-        if(temp[i]+temp[j]==B)
+        s1.push(t1);
+        t1 = t1->left;
+    }
+    while(t2!=NULL)
+    {
+        s2.push(t2);
+        t2 = t2->right;
+    }
+    t1 = s1.top();t2 = s2.top();
+    while(t1->val<t2->val)
+    {
+        if(t1->val + t2->val == B)
         return 1;
-        if(temp[i]+temp[j]<B)
-        i++;
+        if(t1->val + t2->val < B)
+        {
+            s1.pop();
+            t1 = t1->right;
+            while(t1)
+            {
+                s1.push(t1);
+                t1 = t1->left;
+            }
+            t1 = s1.top();
+        }
         else
-        j--;
+        {
+            s2.pop();
+            t2 = t2->left;
+            while(t2)
+            {
+                s2.push(t2);
+                t2 = t2->right;
+            }
+            t2 = s2.top();
+        }
     }
     return 0;
 }
